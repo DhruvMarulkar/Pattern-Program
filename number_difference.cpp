3abfd913c43aea4c9d60a888db9fab505779c921
404 NOT FOUND <dnpmarulkar@gmail.com>
2025-03-22 18:15:29 +0530

    Update number_difference.cpp

--- number_difference.cpp
+++ number_difference.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-    int n = 6;
-    int a = 1;
-    for(int i=1;i<=n;i++)
+    int n;
+    cin>>n;
+    int odd = 1;
+    int even = 2;
+    int digit_counter = 1;
+    for(int i=0;i<=n;i++)
     {
         for(int j=0;j<i;j++)
         {
-            cout<<a<<" ";
-            a += 2;
+            if(i%2!=0)
+            {
+             cout<<odd<<" ";
+             odd += 2;
+             digit_counter += 1;
+            }
+            else
+            {
+             cout<<even<<" ";
+             even += 2;
+              digit_counter += 1;
+            }
         }
         cout<<endl;
     }
